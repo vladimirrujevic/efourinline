a2d95ef80554cf6fcb2901850de96869c21ca6e3
Vladimir Rujević <vladimir.rujevic@student.um.si>
2016-12-05 22:52:13 +0100

    Igralna površina
    Igralna površina se prilagaja velikosti glavneg okna

--- js/game.js
+++ js/game.js
@@ -7,22 +7,35 @@ var start = function () {
 var updateGameUi = function () {
   var w = $('.wrap').height(),
     i = $('.igralci').height(),
-    ig = w - i;
+    ig = w - i,
+    igw = $('.igralci').width(), 
+    h = 0,
+    m = 0;
   $('.igralnaP').height(ig);
+  if(igw > ig){
+    h = Math.round(ig/6);
+  } else {
+    h = Math.round(igw/7);
+  }
+  $('.polje').height(h);
+  $('.polje').width(h);
+  /*m = Math.round(igw/4 - h/2);
+  $('.igralnaP').css("margin-left", m + "px");*/
 };
 
 var drawPolje = function () {
-  var i, j, b = $("<br/>");
-  var poljeI = [];
+  var i, j;
+  var poljeI = new Array(6);
   for(i = 0; i<6; i++){
-    poljeI[i] = [];
-  }
-  for(i = 0; i<6; i++){
-    for(j = 0; i<7; i++){
-      poljeI[i][j] = $("<div/>");
-      poljeI[i][j].addClass("polje");
-      igralnaP.append(poljeI[i][j]);
+    poljeI[i] = new Array(7);
+    for(j = 0; j<7; j++){
+      var d = $("<div/>");
+      d.addClass("polje");
+      d.attr("id", i + "|" + j);
+      poljeI[i][j] = d;
+      igralnaP.append(d);
     }
-    igralnaP.append(b);
+    igralnaP.append($("<br/>"));
   }
+  updateGameUi();
 };
